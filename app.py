41422e9a5cb794d60edf4c1a01030024cc856698
Wei Wei <wei0496@bu.edu>
2017-03-17 15:14:13 -0400

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ app.secret_key = 'super secret string'  # Change this!
 
 #These will need to be changed according to your creditionals
 app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'Mm224069'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'YOURPASSWORD'
 app.config['MYSQL_DATABASE_DB'] = 'photoshare'
 app.config['MYSQL_DATABASE_HOST'] = '127.0.0.1'
 mysql.init_app(app)
